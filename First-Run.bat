9ecf17419c10e10468cccf010ee9c2d9b31e54f0
Mosle963 <mohammad.soleman.963@gmail.com>
2024-07-14 19:14:07 +0300

    first upload

---
+++ First-Run.bat
@@ -0,0 +1,2 @@
+pip install virtualenvwrapper-win && mkvirtualenv SVU_S23_PR601_mohammad_167819_safaa_116845_virtual && pip install -r requirements.txt && py manage.py makemigrations && py manage.py migrate && py manage.py runserver 
+
